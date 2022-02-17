bf0994da685814de0e897eac784c7532bd148d82
dvmaker <97804789+dvmaker@users.noreply.github.com>
2022-02-17 18:28:55 -0300

    Add files via upload

---
+++ src/help1.js
@@ -0,0 +1,30 @@
+const help1 = (prefix) => {
+	return `
+	
+  ~  TdT BOT
+
+
+ 👑  ~ ${prefix}nome
+
+
+ 👑  ~ ${prefix}subir
+ 
+ 
+ 👑  ~ ${prefix}spampreparar
+ 
+
+ 👑  ~ ${prefix}divupreparar
+
+
+ 👑  ~ ${prefix}divuagr
+
+
+ 👑  ~ ${prefix}open
+ 
+ 
+ 👑  ~ ${prefix}close
+
+ `
+}
+
+exports.help1 = help1
