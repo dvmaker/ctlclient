5a4ed7e40030ab458615a3294d3a4efe393fef7a
dvmaker <97804789+dvmaker@users.noreply.github.com>
2022-02-27 14:49:05 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const {
     GroupSettingChange
 } = require('@adiwajshing/baileys')
 const { color, bgcolor } = require('./lib/color')
-const { help } = require('./src/help'))
+const { help } = require('./src/help')
 const { wait, simih, getBuffer, h2k, generateMessageID, getGroupAdmins, getRandom, banner, start, info, success, close } = require('./lib/functions')
 const { fetchJson, fetchText } = require('./lib/fetcher')
 const fs = require('fs')
