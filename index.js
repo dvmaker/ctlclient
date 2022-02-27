0d0aab382e44f09c69d210ad0a477d0635b42865
dvmaker <97804789+dvmaker@users.noreply.github.com>
2022-02-27 14:47:03 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const {
 } = require('@adiwajshing/baileys')
 const { color, bgcolor } = require('./lib/color')
 const { help } = require('./src/help')
+const welkom = JSON.parse(fs.readFileSync('./src/welkom.json'))
 const { wait, simih, getBuffer, h2k, generateMessageID, getGroupAdmins, getRandom, banner, start, info, success, close } = require('./lib/functions')
 const { fetchJson, fetchText } = require('./lib/fetcher')
 const fs = require('fs')
