959268b97c99de32cf3b5496c0459a9a81607ce5
dvmaker <97804789+dvmaker@users.noreply.github.com>
2022-03-05 13:30:06 -0300

    Add files via upload

---
+++ ctl client/msgs/spamalvo.js
@@ -0,0 +1,22 @@
+const spamalvo = (alvospam) => {
+	return `
+	
+  👑  ~ CTL CLIENT
+
+
+  👑🚩️  *ATAQUE de DENUNCIA*  👑🚩
+  
+
+  ✅  ~  *ENVIE UMA MENSAGEM O ALVO, DENUNCIE 15 VEZES E DEPOIS DÊ BLOCK NO ALVO!!* 
+  
+
+  ⟠ 1️⃣: https://api.whatsapp.com/send/?phone=+${alvospam}&text=%20~%20%20👑%20CTL%20CLIENT
+
+
+  🔥 ⟩⟩ *Prints no meu privado!*
+  
+  
+⠀⠀⠀⠀`
+}
+
+exports.spamalvo = spamalvo
