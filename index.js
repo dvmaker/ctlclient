2138c8ad4fab57f4f96a6f0f91e0e336779f6ddd
dvmaker <97804789+dvmaker@users.noreply.github.com>
2022-03-07 00:04:03 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -147,22 +147,22 @@ ctlclient.sendMessage(`${num.split('@')[0]}@s.whatsapp.net`, teks2, MessageType.
 	ctlclient.on('chat-update', async (mek) => {
 		try {
 		
-
+/*
 			if (!mek.hasNewMessage) return
 			mek = mek.messages.all()[0]
 			if (!mek.message) return
 			if (!mek.key.fromMe) return
 			if (mek.key && mek.key.remoteJid == 'status@broadcast') return
 			mek.message = (Object.keys(mek.message)[0] === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
-
+*/
 			
-/*
+
                                     if (!mek.hasNewMessage) return
                                     mek = mek.messages.all()[0]
 			if (!mek.message) return
 			if (mek.key.fromMe) return
 			if (mek.key && mek.key.remoteJid == 'status@broadcast') return
-*/
+
 
 			global.prefix
 			global.blocked
